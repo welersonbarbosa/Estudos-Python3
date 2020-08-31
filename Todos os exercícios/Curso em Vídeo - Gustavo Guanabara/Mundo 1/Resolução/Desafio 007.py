15e499b06f78576be9caa8767a69cc9f885ad525
RayaneGomes97 <62166753+RayaneGomes97@users.noreply.github.com>
2020-08-31 17:04:18 -0300

    Create Desafio 007.py

---
+++ Todos os exercícios/Curso em Vídeo - Gustavo Guanabara/Mundo 1/Resolução/Desafio 007.py
@@ -0,0 +1,7 @@
+# 07 - Faça um algoritmo que leia as duas notas de um aluno, calcule e mostre a sua média
+
+print("Calculadora")
+n = float(input("Digite sua primeira nota:"))
+n2 = float(input("Digite sua segunda nota:"))
+s = (n + n2) / 2
+print("A media entre {} e {} é igual a {}".format(n,n2,s))
