2ec2029499a9ba1e859c4415e14e727dc050dece
RayaneGomes97 <62166753+RayaneGomes97@users.noreply.github.com>
2020-08-31 17:07:45 -0300

    Create Desafio 011.py

---
+++ Todos os exercícios/Curso em Vídeo - Gustavo Guanabara/Mundo 1/Resolução/Desafio 011.py
@@ -0,0 +1,7 @@
+print("Vamos pintar! ")
+l = float(input("Digite a largura de sua parede: "))
+a = float(input("Digite a altura de sua parede: "))
+m2 = l*a
+print("Sua parede possui {} largura x {} altura, temos então {}m².".format(l,a,m2))
+tinta = m2 / 2
+print("Para pintar sua parede iremos utilizar {} litros de tinta. ".format(tinta))
