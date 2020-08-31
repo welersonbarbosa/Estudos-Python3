615533d99fd27c1babaf170cfe223eca9da81146
RayaneGomes97 <62166753+RayaneGomes97@users.noreply.github.com>
2020-08-31 17:03:05 -0300

    Update Desafio 003.py

--- Todos os exercícios/Curso em Vídeo - Gustavo Guanabara/Mundo 1/Resolução/Desafio 003.py
+++ Todos os exercícios/Curso em Vídeo - Gustavo Guanabara/Mundo 1/Resolução/Desafio 003.py
@@ -1,8 +1,7 @@
 # 003 - Crie um programa que leia dois números e mostre a soma entre eles 
 
-x = float(input('Digite um valor para x : '))
-y = float(input('Digite um valor para y: '))
-
-soma = x + y
-
-print('\nA soma de X e Y é igual a {}'.format(soma))
+print("Calculadora")
+n = float(input("Digite sua primeira nota:"))
+n2 = float(input("Digite sua segunda nota:"))
+s = (n + n2) / 2
+print("A media entre {} e {} é igual a {}".format(n,n2,s))
