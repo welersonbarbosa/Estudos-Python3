eaa4cbead4c04c5ec18fbdaf0797c80b67c6334e
RayaneGomes97 <62166753+RayaneGomes97@users.noreply.github.com>
2020-08-31 17:05:00 -0300

    Create Desafio 008.py

---
+++ Todos os exercícios/Curso em Vídeo - Gustavo Guanabara/Mundo 1/Resolução/Desafio 008.py
@@ -0,0 +1,6 @@
+m = float(input ("Digite o valor desejado: "))
+cm = m * 100
+mm = m * 10
+d = m / 10000
+km = m / 1000
+print ("O Valor digitado em metros é {} \n Centimetros {} cm \n Milimetros {} mm \n Decametros {} dm \n Kilometros {}km \n".format(m, cm, mm, d, km))
