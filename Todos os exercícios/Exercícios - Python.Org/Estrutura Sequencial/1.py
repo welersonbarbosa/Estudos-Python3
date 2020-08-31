9933381bfa36e14ef20b272cbc819ed2f6f2a14e
RayaneGomes97 <raayanegomes97@gmail.com>
2020-08-31 16:52:25 -0300

    ...

--- Todos os exercícios/Exercícios - Python.Org/Estrutura Sequencial/1.py
+++
@@ -1,3 +0,0 @@
-# Faça um Programa que mostre a mensagem "Alo mundo" na te
-
-print ('Alo mundo!')
--- Todos os exercícios/Exercícios - Python.Org/Estrutura Sequencial/3.py
+++
@@ -1,8 +0,0 @@
-# 3 - Faça um Programa que peça dois números e imprima a soma
-
-n1 = int(input('Insira um número para n1: '))
-n2 = int(input('Insira um número para n2: '))
-s = n1 + n2
-
-print(' A soma do número {} e {}  é igual a {}'.format(n1,n2,s))
-
